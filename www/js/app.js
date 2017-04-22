95f7b99c4601f2b706b95e68c9024c1eeb11ee81
Søren Raarup <soren@rasor.dk>
2017-04-22 13:47:24 +0200

    ui-leaflet replaces angular-leaflet-directive

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'leaflet-directive'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','nemLogging','ui-leaflet'])
 
 .run(['$ionicPlatform', function($ionicPlatform) {
   $ionicPlatform.ready(function() {
