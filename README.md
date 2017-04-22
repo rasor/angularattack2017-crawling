c1a03d29196eb622b5e047f23945068e12ee9a7e
Søren Raarup <soren@rasor.dk>
2017-04-22 12:52:29 +0200

    added angular-leaflet-directive

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# angularattack2017-crawling" 
+# angularattack2017-crawling 
+
+# Demo
+http://crawling.2017.angularattack.io/ and https://angularattack2017-crawling.herokuapp.com/
+
+# Credits
+This app was based on a sample from [Ahmed Alparslan Özdemir](https://github.com/alparslanahmed/MekanBul).  Thank you.
+
+Uses the [Angular Leaflet Directive](https://tombatossals.github.io/angular-leaflet-directive/) by [David Rubert](https://github.com/tombatossals).
+
+Of course, it wouldn't exist without the fine folks from [Drifty](http://www.drifty.com) and the [Ionic Framework Team](http://ionicframework.com).
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,46 @@
 angular.module('starter.controllers', [])
 
-.controller('MapCtrl', ['$scope', function($scope) {}])
+.controller('MapCtrl', ['$scope', function($scope) {
+
+  /**
+  * Once state loaded, get put map on scope.
+  */
+  // $scope.$on("$stateChangeSuccess", function() {
+  //   alert('hej');
+  // });
+
+  $scope.map = {
+    defaults: {
+        //haritayı alacağımız sunucu
+        //tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        //maxZoom: 18,
+        //zoomControlPosition: 'bottomleft'
+    },
+    //haritanın merkezi
+    center: {
+        lat: 40,
+        lng: 29,
+        zoom: 12
+    }
+  };
+
+    // angular.extend($scope, {
+    //     defaults: {
+    //         scrollWheelZoom: false
+    //     }
+    // });
+
+  // angular.extend($scope, {
+  //     defaults: {
+  //         scrollWheelZoom: false,
+  //         attributionControl: true,
+  //         tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  //         tileLayerOptions: {
+  //             attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //         },
+  //     }
+  // });  
+}])
 
 .controller('ChatsCtrl', ['$scope', 'Chats', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
