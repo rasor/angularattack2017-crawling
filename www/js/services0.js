62dd2e1652c51bc5594416ee25794babe48f3ce7
Søren Raarup <soren@rasor.dk>
2017-04-23 08:07:37 +0200

    - lastText removed from services - services backedup

--- www/js/services0.js
+++ www/js/services0.js
@@ -10,6 +10,7 @@ angular.module('starter.services', [])
   var places = [{
     id: 0,
     name: 'Ben Sparrow',
+    lastText: 'You on your way?',
     face: 'img/ben.png',
     address: '125 High Street, Reading, RG6 1PS',
     rating: 3,
@@ -21,18 +22,22 @@ angular.module('starter.services', [])
   }, {
     id: 1,
     name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
     face: 'img/max.png',
   }, {
     id: 2,
     name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
     face: 'img/adam.jpg',
   }, {
     id: 3,
     name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
     face: 'img/perry.png',
   }, {
     id: 4,
     name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
     face: 'img/mike.png',
   }];
 
