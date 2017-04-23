9e8177b677bdcc0acf990ac53e7cabb1fda855cc
Søren Raarup <soren@rasor.dk>
2017-04-23 07:55:15 +0200

    added extra props to one person

--- www/js/services.js
+++ www/js/services.js
@@ -3,33 +3,42 @@ angular.module('starter.services', [])
 
 .factory('Places', function() {
   // Might use a resource here that returns a JSON array
+  // https://www.mockaroo.com/api/docs
+  // http://www.generatedata.com/
 
   // Some fake testing data
   var places = [{
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'img/ben.png'
+    face: 'img/ben.png',
+    address: '125 High Street, Reading, RG6 1PS',
+    rating: 3,
+    venueTypes: ['Hotel', 'Restarant'],
+    coords: {
+        lat: 51.455041,
+        lng: -0.9690884
+    }
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    face: 'img/max.png',
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
+    face: 'img/adam.jpg',
   }, {
     id: 3,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    face: 'img/perry.png',
   }, {
     id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    face: 'img/mike.png',
   }];
 
   return {
